04919b2fa115d5b92a97cc5187d3e9e177326212
22bcsc04aryanshi <cse.22bcsc04@silicon.ac.in>
2025-08-22 11:35:51 +0530

    Initial commit

---
+++ my-dashboard/src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        title: "Total Orders",
+value: 862,
+timestamp: 1754476592000
+
+
+title: "Ordered Items This Week",
+value: 156,
+timestamp: 1754641532000
+
+
+title: "Return Orders",
+value: 12,
+timestamp: 1754562992000
+
+
+title: "Fulfilled Orders This Week",
+value: 124,
+timestamp: 1754627132000
+    </div>
+  )
+};
+
+export default Card;
